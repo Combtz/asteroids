e296abd4c47d6452b143b6e57e7d2867a796c824
Andy Lai <Andy04Lai@gmail.com>
2024-09-01 13:51:09 +0930

    basic screen limited to 60 fps

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+        
+        #Framerate limiter
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
