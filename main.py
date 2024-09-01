11676bad82c754fbb2208f66dae90746f9615d51
Andy Lai <Andy04Lai@gmail.com>
2024-09-02 00:20:55 +0930

    Asteroids working, no collision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -12,18 +14,38 @@ def main():
     clock = pygame.time.Clock()
 
     dt = 0
+    updatable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable)
+
+    Player.containers = (updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    asteroid_field = AsteroidField()    
+
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for thing in updatable:
+            thing.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+        
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         
         #Framerate limiter
